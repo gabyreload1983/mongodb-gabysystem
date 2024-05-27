69f4d79ed904687d8478c5a5c6997179c45958d8
Gabriel Godoy <gabyreload@gmail.com>
2024-05-26 23:49:26 -0300

    Update init-db.js

--- mongo-init-scripts/init-db.js
+++ mongo-init-scripts/init-db.js
@@ -1,9 +1,9 @@
-db = db.getSiblingDB("mongo-gabysystem");
+db = db.getSiblingDB("api-gabysystem");
 
 db.createUser({
   user: process.env.MONGO_INITDB_ROOT_USERNAME,
   pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
-  roles: [{ role: "readWrite", db: "mongo-gabysystem" }],
+  roles: [{ role: "readWrite", db: "api-gabysystem" }],
 });
 
 db.createCollection("users");
